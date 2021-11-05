ef4b3b61eea49921d7e0b666dfdac7c40924be62
tikak <tika.kandel.06@gmail.com>
2021-11-05 13:24:45 +1100

    adeded Search function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import React, {useState, useEffect} from 'react';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
+import {Jumbotron, Container, Col, Form, Button, Card, CardColumns} from 'react-bootstrap';
+import {searchGoogleBooks} from '../utils/API';
+import {saveBookIds, getSavedBookIds} from '../utils/localStorage';
+import {SAVE_BOOK} from '../gql/mutations';
+import {useMutation} from '@apollo/cliens'
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -65,17 +65,30 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+        const {data} = await saveBook({
+                variables: {input: bookToSave}
+            });
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
+            if (error) {
+                throw new Error('Something went wrong!');
+            }
+
+            setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+        } catch (err) {
+            console.error(err);
+        }
+    //   const response = await saveBook(bookToSave, token);
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   // if book successfully saves to user's account, save book id to state
+    //   setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    // } catch (err) {
+    //   console.error(err);
+    // }
   };
 
   return (
